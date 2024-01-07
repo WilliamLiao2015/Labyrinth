f018e4b76aee08d8301ef81d93843ccb182c9492
WilliamLiao2015 <williamliao2015@gmail.com>
2024-01-07 14:35:50 +0800

    Made server and client use ip address from command line arguments

--- client.c
+++ client.c
@@ -47,16 +47,18 @@ void game_loop(int sockfd) {
 }
 
 
-int main() {
+int main(int argc, char **argv) {
     int sockfd;
     struct sockaddr_in servaddr;
+
+	if (argc != 2) err_quit("usage: server <IPaddress>");
 
     sockfd = Socket(AF_INET, SOCK_STREAM, 0);
 
     bzero(&servaddr, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
     servaddr.sin_port = htons(SERV_PORT);
+    Inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
 
 	Connect(sockfd, (SA *) &servaddr, sizeof(servaddr));
 
--- server.c
+++ server.c
@@ -84,17 +84,19 @@ void *accept_connection(void *arg) {
 }
 
 
-int main() {
+int main(int argc, char **argv) {
     int listenfd;
     struct sockaddr_in servaddr;
     pthread_t thread_ids[MAX_PLAYER] = {0};
+
+	if (argc != 2) err_quit("usage: server <IPaddress>");
 
     listenfd = Socket(AF_INET, SOCK_STREAM, 0);
 
     bzero(&servaddr, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
     servaddr.sin_port = htons(SERV_PORT);
+    Inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
 
     Bind(listenfd, (SA *)&servaddr, sizeof(servaddr));
     Listen(listenfd, LISTENQ);
