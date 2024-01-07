1891d96b4fef3a06a3309610a33dcb60d79ea6fc
WilliamLiao2015 <williamliao2015@gmail.com>
2024-01-08 01:57:53 +0800

    Added victory scene and slightly modified defeated scene

---
+++ stages/victory.c
@@ -0,0 +1,22 @@
+#include "../unp.h"
+#include "../types.h"
+#include "../utils.h"
+#include "../stages.h"
+
+
+int VictoryScene(struct StageOption *option) {
+    char recvline[MAXLINE], message[MAXLINE];
+
+    sprintf(
+        message,
+        "<clear>\n"
+        "你擊敗了%s！\n"
+        "獲得 %d 經驗，獲得 %d 金幣\n"
+        "<options>\n",
+        option->battle_info->monster->name,
+        option->battle_info->monster->exp,
+        option->battle_info->monster->reward
+    );
+    Writen(option->connfd, message, strlen(message));
+    option->next = &VictoryScene; // TODO: Plot
+}
